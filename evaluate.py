99c2067ba934b8a0c45c23bb427e99886e6b9454
Marley Robinson <marleycrobinson99@gmail.com>
2021-09-09 22:02:43 -0500

    work on evaluate and feature eng

--- evaluate.py
+++ evaluate.py
@@ -37,7 +37,7 @@ def lin_reg(df, x, y):
     model.fit(df[[x]], df[y])
 
     # Creating ŷ predictions column
-    df['ŷ'] = model.predict(df[[x]])
+    df['yhat'] = model.predict(df[[x]])
 
     # Create baseline predictions column
     df['baseline'] = df[y].mean()
@@ -108,29 +108,55 @@ def better_than_baseline(y, ŷ):
 
 ###################################         AUTO-FEATURE SELECTION         ###################################
 
-def kbest_features(df, target, k, stratify = False):
+def kbest_features(df, target, k, stratify = False, show_scores = False, scaler_type = StandardScaler()):
     '''
-    df : Unscaled dataframe
-    target : Target variable of df
-    k : Number of features to select
-    stratify : Default is false. If true, stratifies during the train/test split
-    '''
+    Takes a dataframe and uses SelectKBest to select for
+    the most relevant drivers of target.
     
+    Parameters:
+    -----------
+    df : Unscaled dataframe
+
+    target : Target variable of df
+
+    k : Number of features to select
+
+    stratify : No stratification by default. If stratify = true, 
+            stratifies for the target during the train/test split
+
+    show_scores : If true, outputs a dataframe containing the top (k) features
+            and their respective f-scores.
+
+    scaler_type : Default is StandardScaler, determines the type 
+            of scaling applied to the df before
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Output:
+    -------------
+    features : A list of features of (k) length that SelectKBest has selected to be the
+    main drivers of the target.
+
+    fs_sorted : A sorted dataframe that combines each feature with its respective score.
+    '''
+
     # only selects numeric cols and separates target
     X = df[[col for col in df.columns if df[col].dtype != object]].drop(columns = target)
     y = df[target]
-
+    
     # train, test split checking for stratify
     if stratify == True:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, 
                                                             random_state=123,
                                                             stratify=df[target])
-    else:
+    elif stratify == False:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2,
                                                             random_state = 123)
 
     # scaling data
-    scaler = StandardScaler()
+    if scaler_type == StandardScaler():
+        scaler = StandardScaler()
+    else:
+        scaler = scaler_type
 
     # fitting scaler to each split
     X_train_scaled = scaler.fit_transform(X_train)
@@ -140,29 +166,84 @@ def kbest_features(df, target, k, stratify = False):
     kbest = SelectKBest(f_regression, k= k)
     
     # fitting object
-    kbest.fit(x_scaled, y)
+    kbest.fit(X_train_scaled, y_train)
     
     # assigning features to var
-    features = x.columns[kbest.get_support()]
+    features = X.columns[kbest.get_support()]
+    if show_scores == True:
+        # getting feature scores
+        scores = kbest.scores_[kbest.get_support()]
+
+        # creating zipped list of feats and their scores
+        feat_scores = list(zip(features, scores))
     
-    return features
+        fs_df = pd.DataFrame(data = feat_scores, columns= ['Feat_names','F_Scores'])
+    
+        fs_sorted = fs_df.sort_values(['F_Scores','Feat_names'], ascending = [False, True])
+
+        return fs_sorted
+    else:
+        return list(features)
 
 
-def rfe_features(x_scaled, x, y, n, model = LinearRegression()):
+def rfe_features(df, target, n, stratify = False, est_model = LinearRegression(), scaler_type = StandardScaler()):
     '''
-    X_scaled : Takes in a scaled dataframe of features not including the target
-    X : Unscaled dataframe without target feature
-    Y : Takes in an an array containing the target
-    N : Number of features to whittle down to
-    Model: Which model type to use for RFE
+    Takes a dataframe and uses Recursive Feature Elimination to select for
+    the most relevant drivers of target.
+    
+    Parameters:
+    -----------
+    df : Unscaled dataframe
+    
+    target : Target variable of df
+    
+    n : Number of features to select
+    
+    stratify : No stratification by default. If stratify = true, 
+            stratifies for the target during the train/test split
+            
+    est_model : Defailt is LinearRegression, determines the estimator
+            used by the RFE function
+    
+    scaler_type : Default is StandardScaler, determines the type 
+            of scaling applied to the df before
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Output:
+    -------------
+    A list of features of (n) length that RFE has selected to be the
+    main drivers of the target.
     '''
+    # only selects numeric cols and separates target
+    X = df[[col for col in df.columns if df[col].dtype != object]].drop(columns = target)
+    y = df[target]
+
+    # train, test split checking for stratify
+    if stratify == True:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, 
+                                                            random_state=123,
+                                                            stratify=df[target])
+    elif stratify == False:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2,
+                                                            random_state = 123)
+    
+    # scaling data
+    if scaler_type == StandardScaler():
+        scaler = StandardScaler()
+    else:
+        scaler = scaler_type
+    
+    # fitting scaler to each split
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    
     # creating RFE object with {n} selected features
-    rfe = RFE(estimator= model, n_features_to_select=n)
+    rfe = RFE(estimator= est_model, n_features_to_select=n)
     
     # fitting object
-    rfe.fit(x_scaled, y)
+    rfe.fit(X_train_scaled, y_train)
     
     # assigning features to var
-    features = x.columns[rfe.get_support()]
+    features = X.columns[rfe.get_support()]
     
-    return features
+    return list(features)
